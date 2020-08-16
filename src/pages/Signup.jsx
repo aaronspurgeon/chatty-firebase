4b57edec89daba8e4e723e4107ada569e34dd193
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-16 17:56:24 -0400

    added functions to signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { signup } from '../helpers/auth';
 
 export default function Signup(props) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+    // const [signup, setSignup] = useState({
+    //     error: null,
+    //     email: '',
+    //     password: ''
+    // })
+
+    const handleChange = (e) => {
+        setEmail({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setError({ error: '' })
+        try {
+            await signup(email, password);
+        } catch (error) {
+            setError({ error: error.message })
+        }
+    }
+
     return (
         <div>
-            <form onSubmit={props.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <h1>
                     Sign Up to
           <Link to="/">Chatty</Link>
                 </h1>
                 <p>Fill in the form below to create an account.</p>
                 <div>
-                    <input placeholder="Email" name="email" type="email" onChange={props.handleChange} value={props.email}></input>
+                    <input placeholder="Email" name="email" type="email" onChange={handleChange} value={email}></input>
                 </div>
                 <div>
-                    <input placeholder="Password" name="password" onChange={props.handleChange} value={props.password} type="password"></input>
+                    <input placeholder="Password" name="password" onChange={handleChange} value={password} type="password"></input>
                 </div>
                 <div>
-                    {props.error ? <p>{props.error}</p> : null}
+                    {error ? <p>{error}</p> : null}
                     <button type="submit">Sign up</button>
                 </div>
                 <hr></hr>
