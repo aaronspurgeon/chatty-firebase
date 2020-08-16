535ded5b6ddaf642114086da99b140aa6a328938
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-16 17:28:47 -0400

    Route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component, useState } from "react";
 import {
   Route,
   BrowserRouter as Router,
@@ -12,8 +12,41 @@ import Login from './pages/Login';
 import { auth } from './services/firebase';
 import "./App.css";
 
+const PrivateRoute = ({ component: Component, authenticated, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+      }
+    />
+  )
+}
+
+const PublicRoute = ({ component: Component, authenticated, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authenticated === false
+        ? <Component {...props} />
+        : <Redirect to='/chat' />
+      }
+    />
+  )
+}
+
 function App() {
-  return <div className="App"></div>;
+  const [isLoading, setIsLoading] = useState(true)
+  const [isAuth, setIsAuth] = useState(false)
+  return isLoading === true ? <h1>Loading...</h1> : (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/chat' authenticated={isAuth} component={Chat} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
