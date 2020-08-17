68064c88dd307ac5dbe63bacf8799d4d4541fc9b
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-17 15:41:39 -0400

    pausing, may go a different route

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,6 @@
 import firebase from "firebase";
 
-const config = {
-  apiKey: "AIzaSyANvHuaXxldAlYggzflgkHmoZKeGnFqNrM",
-  authDomain: "chatty-app-8839c.firebaseapp.com",
-  databaseURL: "https://chatty-app-8839c.firebaseio.com",
-};
+const config = {};
 
 firebase.initializeApp(config);
 export const auth = firebase.auth;
